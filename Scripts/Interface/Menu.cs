3c2018504102361c1417d5c81291bac26fd8012b
Xellu <83588955+xellu@users.noreply.github.com>
2024-01-07 17:45:03 +0700

    Add RayCast and BlockHighlight objects to Game scene, update Menu scene with new script, and fix typo in player scene

---
+++ Scripts/Interface/Menu.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class Menu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+    //handle button press
+    private void _on_Button_pressed()
+    {
+        GD.Print("Button pressed");    
+    }
+}
