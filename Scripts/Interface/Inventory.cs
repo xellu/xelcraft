9e07182e6e7fef79fa088a28f23f87f2a0beb3ec
Xellu <83588955+xellu@users.noreply.github.com>
2024-01-09 14:13:04 +0700

    Item management

--- Scripts/Interface/Inventory.cs
+++ Scripts/Interface/Inventory.cs
@@ -4,6 +4,16 @@ using System;
 public partial class Inventory : Control
 {
     public bool IsOpen = false;
+    public bool Loaded = false;
+    private ItemList _itemList;
+
+    public InventoryItem[] _inventoryItems = {
+        new InventoryItem("Grass Block", BlockManager.Instance.Grass, "res://Textures/Blocks/grass_block_side.png", "grass_block"),
+        new InventoryItem("Dirt", BlockManager.Instance.Dirt, "res://Textures/Blocks/dirt.png"),
+        new InventoryItem("Cobblestone", BlockManager.Instance.Stone, "res://Textures/Blocks/cobblestone.png")
+        
+    };
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -13,7 +23,19 @@ public partial class Inventory : Control
 	public override void _Process(double delta)
 	{
         if (Visible != IsOpen) {Visible = IsOpen;}
-	}
+
+        if (!Loaded) {
+            //load inventory items
+            _itemList = GetNode<ItemList>("ItemList");
+            _itemList.Clear();
+            foreach (InventoryItem item in _inventoryItems) {
+                _itemList.AddItem(item.Name, item.Texture2);
+            }
+
+            GD.Print("Inventory: Loaded inventory items");
+            Loaded = true;
+        }
+    }
 
     public override void _UnhandledInput(InputEvent @event)
     {
